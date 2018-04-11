0142ed08a811f643a3b997f6146c46a06b611b01
duonghan <20155296@student.hust.edu.vn>
2018-04-11 09:28:17 +0700

    Update rule

--- BookApp/BUS/Rule.cs
+++ BookApp/BUS/Rule.cs
@@ -25,31 +25,94 @@ namespace BookApp.BUS
             }
         }
 
-        private char[] charEng = { 'j', 'z', 'w', 'f' };
-        private char[] vowel = { 'u', 'e', 'o', 'a', 'i', 'ư', 'ơ', 'ê', 'ă', 'â', 'ô' };
-        private string[] firstConsonant = { "nh", "th", "ph", "tr", "ch", "gh", "kh", "ng" };
-        private string[] lastConsonant = { "c", "m", "n", "p", "t", "nh", "ng", "ch" };
-        private const int MAXLENGTH = 6;
+        private string wrongChar = "wjzfWJZF";
+        private string wrongLastCons = "bdklqrsvxBDKLQRSVX";
+        private String allConsoants = "bcdđghklmnpqrstvBCDĐGHKLMNPQRSTV";
+        private string vowel = "ueoaiươêăâô";
+        //private char[] vowel = { 'u', 'e', 'o', 'a', 'i', 'ư', 'ơ', 'ê', 'ă', 'â', 'ô' };
+        private string[] correctFirstDbCons = { "nh", "th", "ph", "tr", "ch", "gh", "kh", "ng" };
+        private string[] correctLastDbCons = { "nh", "ng", "ch" };
+        private const int MAXLENGTH = 7;
+
+        private bool checkLength(string str)
+        {
+            if (0 < str.Length && str.Length <= 7)
+                return true;
+            return false;
+        }
 
         //Return true if string not contain any english character
-        public bool checkCharEng(string str)
+        private bool checkWrongChar(string str)
         {
-            foreach(char ch in charEng)
+            for (int i = 0; i < str.Length; i++)
             {
-                if (str.Contains(ch)) return false;
+                if (wrongChar.Contains("" + str[i])) return false;
             }
+
+            if (wrongLastCons.Contains("" + str[str.Length - 1])) return false;
 
             return true;
         }
 
-        public bool checkFirstCoupleConsonant(string str)
+        private bool checkDbCons(string str)
         {
-            if (str.Length > 2)
+            if (allConsoants.Contains(str[0]) && allConsoants.Contains(str[1]))
             {
-                
+                int i = 0;
+                String ch = str[0] + "" + str[1];
+                for (; i < correctFirstDbCons.Length; i++)
+                {
+                    if (ch.ToUpper().Equals(correctFirstDbCons[i].ToUpper()))
+                        break;
+                }
+
+                if (i > correctFirstDbCons.Length)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
             }
 
-            return true;
+            if (allConsoants.Contains(str[str.Length-1]) && allConsoants.Contains(str[str.Length - 2]))
+            {
+                int i = 0;
+                String ch = str[str.Length - 2] + "" + str[str.Length - 1];
+                for (; i < correctLastDbCons.Length; i++)
+                {
+                    if (ch.ToUpper().Equals(correctLastDbCons[i].ToUpper()))
+                        break;
+                }
+
+                if (i > correctLastDbCons.Length)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+            return false;
+        }
+
+        //Kiem tra l va n
+
+        //Kiem tra cac ten rieng cua nhan vat
+        private bool checkName(String str)
+        {
+            return false;
+        }
+
+        //Total check
+        public bool isCorrect(String str)
+        {
+            return (checkLength(str) && checkWrongChar(str) && checkDbCons(str) && checkName(str));
         }
         
     }
